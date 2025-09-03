fec71f3f2fb20ffeeb9986dbe92422be8b63c3e1
RyderL33 <108831917+RyderL33@users.noreply.github.com>
2025-09-03 12:03:12 -0700

    Update RoomCleaner_RL.java

--- RoomCleaner_RL.java
+++ RoomCleaner_RL.java
@@ -1,133 +1,138 @@
-import java.util.Scanner;  //may be necessary for input
+	import java.util.Scanner;  //may be necessary for input
 
-import javax.swing.JOptionPane;  //may be necessary for input
+	import javax.swing.JOptionPane;  //may be necessary for input
 
-import kareltherobot.*;
+	import kareltherobot.*;
 
-public class Driver implements Directions {
-// declared here so it is visible in all the methods!! 
-// It will be assigned a value in the getInfo method
-	private static Robot roomba; 
-
-
-	// You will add very many variables!!
-	// variables to print
-	int roomArea = 0;
-	int numPiles = 0;
-	int largestPile = 0;
-	double avgPileSIZE = 0;
-	double percentDirty = 0;
-		int numTilesTotal = 0;
-		int numTilesDirty = 0;
-		// to find % dirty, take numTilesDirty divided by numTilesTotal
-	int numBeepers = 0;
-
-	//variables for calculation
-	
-
-	
-	public static void main(String[] args) {
-		// LEAVE THIS ALONE!!!!!!
-		Driver d = new Driver();
-
-  /**
-	 * This section of code gets info from the user in the following order: 1. Ask the user
-	 * which world file they wish to process. Right now, that world file name is
-	 * hardcoded in. 2. Ask the user for the starting location and direction of the
-	 * Robot. A new Robot should be constructed and assigned to the global
-	 * (instance) variable named roomba that is declared on line 10.
-	 * 
-	 * An inelegant way to interact with the user is via the console, using
-	 * System.out.println and a Scanner that scans System.in (input from the
-	 * console). A more elegant way to get user input might include using a
-	 * JOptionPane.
-	 */
-
-    String wrldName = "basicRoom.wld";
-
-		World.readWorld(wrldName);
-    	World.setVisible(true);
-		Robot r = new Robot (7,6,East,100); //set robot location, direction, and # of beepers
-	
-
-    
+	public class Driver implements Directions {
+	// declared here so it is visible in all the methods!! 
+	// It will be assigned a value in the getInfo method
+		private static Robot roomba; 
 
 
-	/** This section will have all the logic that takes the Robot to every location 
-	 * and cleans up all piles of beepers.  Think about ways you can break this
-	 * large, complex task into smaller, easier to solve problems.
-	 */
-
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
+		// You will add very many variables!!
+		
 
 		
+		public static void main(String[] args) {
+			// LEAVE THIS ALONE!!!!!!
+			Driver d = new Driver();
+
+		// variables for calculation or print
+		int roomArea = 0;
+		int numPiles = 0;
+		int largestPile = 0;
+		double avgPileSIZE = 0;
+		double percentDirty = 0;
+			int numTilesTotal = 0;
+			int numTilesEmpty = 0;
+			// to find % dirty, take numTilesDirty divided by numTilesTotal
+		int numBeepers = 0;
+		int roomsizeX = 0;
+		int roomsizeY = 0;
+
+
+
+	/**
+		 * This section of code gets info from the user in the following order: 1. Ask the user
+		 * which world file they wish to process. Right now, that world file name is
+		 * hardcoded in. 2. Ask the user for the starting location and direction of the
+		 * Robot. A new Robot should be constructed and assigned to the global
+		 * (instance) variable named roomba that is declared on line 10.
+		 * 
+		 * An inelegant way to interact with the user is via the console, using
+		 * System.out.println and a Scanner that scans System.in (input from the
+		 * console). A more elegant way to get user input might include using a
+		 * JOptionPane.
+		 */
+
+		String wrldName = "basicRoom.wld";
+
+			World.readWorld(wrldName);
+			World.setVisible(true);
+			Robot r = new Robot (7,6,East,100); //set robot location, direction, and # of beepers
 		
+
+		
+
+
+		/** This section will have all the logic that takes the Robot to every location 
+		 * and cleans up all piles of beepers.  Think about ways you can break this
+		 * large, complex task into smaller, easier to solve problems.
+		 */
+
+			// the line below causes a null pointer exception
+			// what is that and why are we getting it?
+
+
+		// at start of program, get the room size (num of rows and columns)
+		while (r.frontIsClear() == true)
+			{
+				
+				r.move();
+				roomsizeX
+			}
+
+		for (;;)
+		{
+			if (r.frontIsClear() == true)
+				{
+					while (r.nextToABeeper() == true)
+						{
+							r.pickBeeper();
+							numBeepers++;
+						}
+					if (r.nextToABeeper() == false)
+						{
+							numTilesEmpty++;
+						}
+					r.move();
+					numTilesTotal++;
+				}
+			
 			if (r.frontIsClear() == false && r.facingEast() == true)
 				{
 					r.turnLeft();
 					r.move();
 					r.turnLeft();
-					int numTilesTotal++;
+					numTilesTotal++;
 				}	
-			else
-				{
-					r.move();
-					int numTilesTotal++;
-				}
 
 			if (r.frontIsClear() == false && r.facingWest() == true)
 				{
-					for (int a = 0; a <= 3; a++)
+					for (int a = 0; a < 3; a++)
 						{
 							r.turnLeft();
 						}
 					r.move();
-					int numTilesTotal++;
-					for (int b = 0; b <= 3; b++)
+					numTilesTotal++;
+					for (int b = 0; b < 3; b++)
 						{
 							r.turnLeft();
 						}
 				}
-			else
-				{
-					r.move();
-					int numTilesTotal++;
-				}
+		}
 
-			while (r.nextToABeeper() == true)
-				{
-					r.pickBeeper();
-					int numBeepers++;
-				}
-
+				
 			
 
-
-
-
-			
-		
-
-
-	
+				
 
 
 
 
+		/** This method displays the results of cleaning the room.  All of the info
+		 * in the pdf that describes the problem need to be displayed.  You can present
+		 * this info in the console (boring) or you can present using JOptionPane (cool!)
+		 */
 
-  	/** This method displays the results of cleaning the room.  All of the info
-	 * in the pdf that describes the problem need to be displayed.  You can present
-	 * this info in the console (boring) or you can present using JOptionPane (cool!)
-	 */
-
-    // System.out.println("The biggest pile was ");
-	// System.out.println("Number of piles was");
-	// System.out.println("Total number of beepers was ");
-	// System.out.println("Average pile size ");
-	// System.out.println("Area of room was ");
-	// System.out.println("Location of the largest pile ");
-	// System.out.println("Percent dirty was ");
+		// System.out.println("The biggest pile was ");
+		// System.out.println("Number of piles was" + (numTilesTotal - numTilesEmpty));
+		// System.out.println("Total number of beepers was " + numBeepers);
+		// System.out.println("Average pile size " + (double) numBeepers/(numTilesTotal - numTilesEmpty));
+		// System.out.println("Area of room was " numTilesTotal);
+		// System.out.println("Location of the largest pile ");
+		// System.out.println("Percent dirty was " + (double) numTilesEmpty/numTilesTotal);
 
 
 
@@ -135,6 +140,6 @@ public class Driver implements Directions {
 
 
 
-  }
+	}
 
-}
+	}
