caa841edf759fa27a1f0530d9c102cb73898d87d
RyderL33 <108831917+RyderL33@users.noreply.github.com>
2025-10-06 11:36:15 -0700

    Add files via upload

---
+++ ReadData_RL.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class ReadData {
+
+    public void getData()
+    {
+        try
+        {
+        Scanner in = new Scanner (new File("songs2025-2026.csv"))
+        }
+        catch()
+        {
+            
+        }
+    }
+}
