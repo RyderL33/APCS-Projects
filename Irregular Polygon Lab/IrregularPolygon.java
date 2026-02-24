83a92d2c258d98b5136966838019fdbdfc309d7c
RyderL33 <108831917+RyderL33@users.noreply.github.com>
2026-02-24 11:34:09 -0800

    Create IrregularPolygon.java

---
+++ Irregular Polygon Lab/IrregularPolygon.java
@@ -0,0 +1,65 @@
+import java.awt.geom.*; // for Point2D.Double
+import java.util.ArrayList; // for ArrayList
+import java.util.concurrent.TimeUnit;
+
+import gpdraw.*; // for DrawingTool
+
+
+public class IrregularPolygon {
+    private ArrayList<Point2D.Double> myPolygon = new ArrayList<Point2D.Double>();
+
+    // constructor
+    public IrregularPolygon() {}
+
+    // public methods
+    public void add(Point2D.Double aPoint)
+    {
+        // TODO: Add a point to the IrregularPolygon.
+        myPolygon.add(aPoint);
+    }
+
+    public double perimeter() {
+        // TODO: Calculate the perimeter.
+        // distance formula: d = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+        double perimeter = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++)
+        {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % n);
+            double distance = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+            perimeter += distance;
+        }
+        return perimeter;
+    }
+
+    public double area() {
+        // TODO: Calculate the area.
+        if (myPolygon.size() < 3) // checks to see if there are enough data points
+        {
+            return 0.0; //otherwise a full closed shape won't be created, so area = 0.0
+        }
+        double area = 0.0;
+        int n = myPolygon.size();
+        for (int i = 0; i < n; i++) 
+        {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % n);
+            area += p1.x * p2.y - p2.x * p1.y;
+        }
+        return Math.abs(area / 2.0);
+    }
+
+    public void draw()
+    {
+        // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
+        try {
+            // TODO: Draw the polygon.
+            
+            // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
+            DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
+            pen.move(50, 50);
+        } catch (java.awt.HeadlessException e) {
+            System.out.println("Exception: No graphics support available.");
+        }
+    }
+}
